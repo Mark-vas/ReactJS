39260627ae313f85bbc911c1ba67db4449282190
Mark-vas <vasyaev.mark@mail.ru>
2021-04-30 17:01:14 +0300

    !!!

--- static_src/index.js
+++ static_src/index.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-let element = <h1 className="element">Кажется, мы подключили React!</h1>;
+const messages = ['Как у тебя дела?']
+
+// clickOnButton = () => {
+//     messages.push('Нормально')
+// }
+
+// const Message = (props) =>
+//     <div>
+//         <h1>{props.text}</h1>
+//         <button OnClick={this.clickOnButton}></button>
+//     </div>
+
+const Message = (props) => {
+    let clickOnButton = () => {
+        messages.push('Нормально')
+        console.log(messages)
+    }
+    return (
+        <div>
+            <h1>{props.text}</h1>
+            <button onClick={clickOnButton} >Кнопка</button>
+        </div>)
+}
+
+
+
+// const MessageList = (props) => {
+//     return props.messages.map((message) => <Message text={message} />)
+// }
 
 ReactDOM.render(
-    element,
+    <Message text={messages} />,
     document.getElementById('root'),
 );
