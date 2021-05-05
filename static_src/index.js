e072f200d893340436371b58735bf518fd3029fc
Mark-vas <vasyaev.mark@mail.ru>
2021-05-05 11:07:49 +0300

    !!!

--- static_src/index.js
+++ static_src/index.js
@@ -3,18 +3,8 @@ import ReactDOM from 'react-dom';
 
 const messages = ['Как у тебя дела?']
 
-// clickOnButton = () => {
-//     messages.push('Нормально')
-// }
-
-// const Message = (props) =>
-//     <div>
-//         <h1>{props.text}</h1>
-//         <button OnClick={this.clickOnButton}></button>
-//     </div>
-
 const Message = (props) => {
-    let clickOnButton = () => {
+    const clickOnButton = () => {
         messages.push('Нормально')
         console.log(messages)
     }
@@ -24,12 +14,6 @@ const Message = (props) => {
             <button onClick={clickOnButton} >Кнопка</button>
         </div>)
 }
-
-
-
-// const MessageList = (props) => {
-//     return props.messages.map((message) => <Message text={message} />)
-// }
 
 ReactDOM.render(
     <Message text={messages} />,
